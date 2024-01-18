17ffbf3fe76f0a854040ccb8510f882d43c8a6de
fabiodigiacomopepe <fabiodgp20@gmail.com>
2024-01-18 20:11:41 +0100

    Add method to return books list by userId

--- bookstore-java-vue-hastega/src/main/java/com/hastegaex/exercise/bookstorejavavuehastega/repository/UserRepository.java
+++ bookstore-java-vue-hastega/src/main/java/com/hastegaex/exercise/bookstorejavavuehastega/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.hastegaex.exercise.bookstorejavavuehastega.repository;
 import com.hastegaex.exercise.bookstorejavavuehastega.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findById(Integer id);
 }
--- bookstore-java-vue-hastega/src/main/java/com/hastegaex/exercise/bookstorejavavuehastega/service/UserService.java
+++ bookstore-java-vue-hastega/src/main/java/com/hastegaex/exercise/bookstorejavavuehastega/service/UserService.java
@@ -1,11 +1,13 @@
 package com.hastegaex.exercise.bookstorejavavuehastega.service;
 
+import com.hastegaex.exercise.bookstorejavavuehastega.exceptions.UserNotFoundException;
 import com.hastegaex.exercise.bookstorejavavuehastega.model.User;
 import com.hastegaex.exercise.bookstorejavavuehastega.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,18 @@ public class UserService {
     public List<User> getUserList() {
         // Ritorno lista completa di utenti
         return userRepository.findAll();
+    }
+
+    public User getBookListByUserId(Integer id) throws UserNotFoundException {
+        // Salvo il result in modo Optional perchè potrebbe non ritornare nulla
+        Optional<User> result = userRepository.findById(id);
+        // Se il risultato è presente
+        if (result.isPresent()) {
+            // Ritorno l'utente (result.get())
+            return result.get();
+        } else {
+            // Altrimenti lancio eccezione
+            throw new UserNotFoundException("User with ID " + id + ": Not Found");
+        }
     }
 }
