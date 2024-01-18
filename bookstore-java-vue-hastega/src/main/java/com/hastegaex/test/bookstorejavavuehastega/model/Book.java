a1b198dfc4df42962e5890e31c2e2cf69a7d52e6
fabiodigiacomopepe <fabiodgp20@gmail.com>
2024-01-18 12:09:29 +0100

    Created user and book model

---
+++ bookstore-java-vue-hastega/src/main/java/com/hastegaex/test/bookstorejavavuehastega/model/Book.java
@@ -0,0 +1,103 @@
+package com.hastegaex.test.bookstorejavavuehastega.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Il titolo non puo' essere nullo")
+    @Size(max = 255, message = "Il titolo non può essere più di 255 caratteri")
+    private String title;
+
+    @NotBlank(message = "L'autore non puo' essere nullo")
+    @Size(max = 255, message = "L'autore non può essere più di 255 caratteri")
+    private String author;
+
+    @NotBlank(message = "L'ISBN non puo' essere nullo")
+    @Size(max = 13, message = "L'ISBN non può essere più di 13 caratteri")
+    @Column(length = 13, nullable = false, unique = true)
+    private String isbn;
+
+    private String createdAt;
+
+    @Column(nullable = true)
+    private String deletedAt;
+
+    @Column(columnDefinition = "TEXT")
+    private String plot;
+
+    @Min(0)
+    private Integer numberOfCompleteReadings;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(String deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public Integer getNumberOfCompleteReadings() {
+        return numberOfCompleteReadings;
+    }
+
+    public void setNumberOfCompleteReadings(Integer numberOfCompleteReadings) {
+        this.numberOfCompleteReadings = numberOfCompleteReadings;
+    }
+}
---
+++ bookstore-java-vue-hastega/src/main/java/com/hastegaex/test/bookstorejavavuehastega/model/User.java
@@ -0,0 +1,69 @@
+package com.hastegaex.test.bookstorejavavuehastega.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    // Con fetch = FetchType.EAGER carico tutto
+    @ManyToMany(fetch = FetchType.LAZY)
+    private List<Book> books;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
