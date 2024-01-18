ef977e39e08536ca2871f918c49854c9b24379e9
fabiodigiacomopepe <fabiodgp20@gmail.com>
2024-01-18 10:39:05 +0100

    Initial commit

---
+++ bookstore-java-vue-hastega/src/main/java/com/hastegaex/test/bookstorejavavuehastega/BookstoreJavaVueHastegaApplication.java
@@ -0,0 +1,13 @@
+package com.hastegaex.test.bookstorejavavuehastega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreJavaVueHastegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreJavaVueHastegaApplication.class, args);
+	}
+
+}
---
+++ bookstore-java-vue-hastega/src/test/java/com/hastegaex/test/bookstorejavavuehastega/BookstoreJavaVueHastegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hastegaex.test.bookstorejavavuehastega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreJavaVueHastegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
