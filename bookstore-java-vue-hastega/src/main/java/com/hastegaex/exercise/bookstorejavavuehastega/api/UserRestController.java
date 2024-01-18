ccbc6a07987e98c7071614277a82d12a439001d6
fabiodigiacomopepe <fabiodgp20@gmail.com>
2024-01-18 13:00:00 +0100

    Created REST controller for API call on frontend

---
+++ bookstore-java-vue-hastega/src/main/java/com/hastegaex/exercise/bookstorejavavuehastega/api/UserRestController.java
@@ -0,0 +1,28 @@
+package com.hastegaex.exercise.bookstorejavavuehastega.api;
+
+import com.hastegaex.exercise.bookstorejavavuehastega.model.User;
+import com.hastegaex.exercise.bookstorejavavuehastega.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@CrossOrigin
+public class UserRestController {
+    @Autowired
+    private UserService userService;
+
+    // Rotta "/api/v1/users" (GET)
+    @GetMapping
+    public List<User> index() {
+        // Ritorno lista degli utenti
+        return userService.getUserList();
+    }
+}
+
+
