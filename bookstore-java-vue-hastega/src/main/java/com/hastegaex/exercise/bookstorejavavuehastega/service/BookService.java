ee69eccb48f8c1980071cb14b89f9246d862efdf
fabiodigiacomopepe <fabiodgp20@gmail.com>
2024-01-19 17:38:55 +0100

    Changed methods in book and user services

--- bookstore-java-vue-hastega/src/main/java/com/hastegaex/exercise/bookstorejavavuehastega/service/BookService.java
+++ bookstore-java-vue-hastega/src/main/java/com/hastegaex/exercise/bookstorejavavuehastega/service/BookService.java
@@ -1,12 +1,11 @@
 package com.hastegaex.exercise.bookstorejavavuehastega.service;
 
-import com.hastegaex.exercise.bookstorejavavuehastega.exceptions.BookNotFoundException;
 import com.hastegaex.exercise.bookstorejavavuehastega.model.Book;
 import com.hastegaex.exercise.bookstorejavavuehastega.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class BookService {
@@ -14,16 +13,12 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
-    public Book getBookById(Integer id) throws BookNotFoundException {
-        // Salvo il result in modo Optional perchè potrebbe non ritornare nulla
-        Optional<Book> result = bookRepository.findById(id);
-        // Se il risultato è presente
-        if (result.isPresent()) {
-            // Ritorno il libro (result.get())
-            return result.get();
-        } else {
-            // Altrimenti lancio eccezione
-            throw new BookNotFoundException("Book with ID " + id + ": Not Found");
-        }
+    public List<Book> getBookListByUserId(Integer id) {
+        return bookRepository.findAllByUserIdWhereDeletedAtIsNotNull(id);
     }
+
+    public Book getBookListById(Integer id) {
+        return bookRepository.findAllById(id);
+    }
+
 }
