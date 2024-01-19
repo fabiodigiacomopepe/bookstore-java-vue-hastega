5f090de56128e9f03bf976b94dcd7798e73c8cf8
fabiodigiacomopepe <fabiodgp20@gmail.com>
2024-01-19 14:15:51 +0100

    Added path in store

--- bookstore-java-vue-hastega/frontend/src/store.js
+++ bookstore-java-vue-hastega/frontend/src/store.js
@@ -2,6 +2,9 @@ import { reactive } from "vue";
 
 export const store = reactive({
     pathApiUsers: "http://localhost:8080/api/v1/users",
+    pathApiBooks: "http://localhost:8080/api/v1/books/user/",
+    pathApiBookDetail: "http://localhost:8080/api/v1/books/",
+    userIdLogin: 0,
     arrayUsers: [],
     loading: true,
 });
