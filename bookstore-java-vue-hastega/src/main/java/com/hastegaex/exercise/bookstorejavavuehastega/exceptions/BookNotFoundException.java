b462d09cac835a43979b5a1d319e986a584d4da4
fabiodigiacomopepe <fabiodgp20@gmail.com>
2024-01-19 17:35:32 +0100

    Created custom exceptions

--- bookstore-java-vue-hastega/src/main/java/com/hastegaex/exercise/bookstorejavavuehastega/exceptions/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.hastegaex.exercise.bookstorejavavuehastega.exceptions;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
--- bookstore-java-vue-hastega/src/main/java/com/hastegaex/exercise/bookstorejavavuehastega/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.hastegaex.exercise.bookstorejavavuehastega.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
