206d333b04b5facf5a8b040563b5f250a5385559
fabiodigiacomopepe <fabiodgp20@gmail.com>
2024-01-19 17:38:19 +0100

    Added custom method in bookRepository

--- bookstore-java-vue-hastega/src/main/java/com/hastegaex/exercise/bookstorejavavuehastega/repository/BookRepository.java
+++ bookstore-java-vue-hastega/src/main/java/com/hastegaex/exercise/bookstorejavavuehastega/repository/BookRepository.java
@@ -2,9 +2,14 @@ package com.hastegaex.exercise.bookstorejavavuehastega.repository;
 
 import com.hastegaex.exercise.bookstorejavavuehastega.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
-    Optional<Book> findById(Integer id);
+    @Query(value = "SELECT b FROM Book b WHERE b.user.id = :userId AND b.deletedAt IS NULL")
+    List<Book> findAllByUserIdWhereDeletedAtIsNotNull(@Param("userId") Integer userId);
+
+    Book findAllById(Integer id);
 }
