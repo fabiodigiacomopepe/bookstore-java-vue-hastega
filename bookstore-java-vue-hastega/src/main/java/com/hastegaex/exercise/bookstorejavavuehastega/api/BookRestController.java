dcb702e3df595d212141e0e4bd97a611ee4eeefe
fabiodigiacomopepe <fabiodgp20@gmail.com>
2024-01-19 14:13:58 +0100

    Added method to return book by id in rest controller

--- bookstore-java-vue-hastega/src/main/java/com/hastegaex/exercise/bookstorejavavuehastega/api/BookRestController.java
+++ bookstore-java-vue-hastega/src/main/java/com/hastegaex/exercise/bookstorejavavuehastega/api/BookRestController.java
@@ -1,6 +1,8 @@
 package com.hastegaex.exercise.bookstorejavavuehastega.api;
 
+import com.hastegaex.exercise.bookstorejavavuehastega.model.Book;
 import com.hastegaex.exercise.bookstorejavavuehastega.model.User;
+import com.hastegaex.exercise.bookstorejavavuehastega.service.BookService;
 import com.hastegaex.exercise.bookstorejavavuehastega.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,10 +14,20 @@ public class BookRestController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private BookService bookService;
+
     // Rotta "/api/v1/books/user/id" <---(dinamico) (GET)
     @GetMapping("user/{id}")
     public User index(@PathVariable Integer id) {
         // Ritorno lista degli utenti
         return userService.getBookListByUserId(id);
+    }
+
+    // Rotta "/api/v1/books/id" <---(dinamico) (GET)
+    @GetMapping("/{id}")
+    public Book findById(@PathVariable Integer id) {
+        // Ritorno libro
+        return bookService.getBookById(id);
     }
 }
