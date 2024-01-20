b6327512b852beaced36999acb45eb949e60873e
fabiodigiacomopepe <fabiodgp20@gmail.com>
2024-01-20 12:42:38 +0100

    Added method for delete book in BookService

--- bookstore-java-vue-hastega/src/main/java/com/hastegaex/exercise/bookstorejavavuehastega/service/BookService.java
+++ bookstore-java-vue-hastega/src/main/java/com/hastegaex/exercise/bookstorejavavuehastega/service/BookService.java
@@ -6,6 +6,8 @@ import com.hastegaex.exercise.bookstorejavavuehastega.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Service
@@ -38,5 +40,20 @@ public class BookService {
         // Salvo il book
         // Metodo .save salva ciò che riceve. Se i campi nel form mancano, li lascia vuoti (non si comporta come update)
         return bookRepository.save(bookToEdit);
+    }
+
+    public Book deleteBook(Integer id, String formData) throws BookNotFoundException {
+        // Provo a prendere book in base a id
+        Book bookToDelete = getBookById(id);
+
+        // Converto stringa eliminado caratteri speciali
+        String dataDecoded = URLDecoder.decode(formData, StandardCharsets.UTF_8);
+        String dataDecodedFinal = dataDecoded.replace("=", "");
+
+        // Valorizzo con i setter i vari parametri passando quelli ricevuti dal form
+        bookToDelete.setDeletedAt(dataDecodedFinal);
+        // Salvo il book
+        // Metodo .save salva ciò che riceve. Se i campi nel form mancano, li lascia vuoti (non si comporta come update)
+        return bookRepository.save(bookToDelete);
     }
 }
